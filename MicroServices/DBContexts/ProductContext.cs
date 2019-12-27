9171beaa10ba0874d18acfccf088b9cc3ca7499d
gkhnkrcr <53599327+gkhnkrcr@users.noreply.github.com>
2019-12-27 14:41:34 +0300

    Add files via upload
    first code

---
+++ MicroServices/DBContexts/ProductContext.cs
@@ -0,0 +1,48 @@
+﻿using MicroServices.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroServices.DBContexts
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+
+                new Category
+                {
+                    Description = "Electronic Items",
+                    Id = 1,
+                    Name = "Electronics"
+
+                },
+                new Category
+                {
+                    Description = "Dresses",
+                    Id = 1,
+                    Name = "Clothes"
+
+                },
+                new Category
+                {
+                    Description = "Grocery Items",
+                    Id = 1,
+                    Name = "Grocery"
+
+                }
+                );
+        }
+    }
+}
---
+++ MicroServices/Repository/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using MicroServices.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroServices.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+
+        Product GetProductByID(int ProductId);
+
+        void InsertProduct(Product product);
+
+        void DeleteProduct(int ProductId);
+
+        void UodateProduct(Product product);
+
+        void Save();
+    }
+}
